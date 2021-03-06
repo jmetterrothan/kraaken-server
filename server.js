8c0f6cb56ec4f60e45860c5b31a3d4599229b78d
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2021-03-06 19:11:12 +0100

    feat: handle history

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use("/api", api);
 const server = new http.Server(app);
 const port = process.env.PORT || 4200;
 
+const rooms = new Map();
+
 const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
@@ -24,19 +26,43 @@ io.on("connection", (socket) => {
   console.log(`Socket ${socket.id} connected`);
 
   socket.on("join_room", (roomId) => {
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, []);
+    }
+
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user_joined_room", uuid);
 
     console.log(`Joined room ${roomId}`);
   });
 
+  socket.on("sync_with_room", (roomId) => {
+    const events = rooms.get(roomId);
+
+    if (events) {
+      events.forEach(({ type, event }) => {
+        socket.emit(type, event);
+      });
+    }
+  });
+
   socket.on("event", ({ roomId, event: { type, ...event } }) => {
     io.to(roomId).emit(type, event);
+
+    const events = rooms.get(roomId);
+
+    if (events) {
+      events.push({ type, event });
+    }
   });
 
   socket.on("disconnecting", () => {
     socket.rooms.forEach((roomId) => {
       socket.to(roomId).broadcast.emit("user_left_room", uuid);
+      // clean up when last user leaves
+      if (io.sockets.adapter.rooms.get(roomId).size - 1 === 0) {
+        rooms.delete(roomId);
+      }
     });
   });
 
