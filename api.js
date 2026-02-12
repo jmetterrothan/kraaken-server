443685fc710fc285c14d162fb8b1105a7fbdc03a
Jérémie Metter-Rothan <jmetterrothan@gmail.com>
2026-02-12 11:10:36 +0100

    feat: improve project struct

--- api.js
+++ api.js
@@ -9,10 +9,12 @@ const schemas = require("./schemas");
 
 const api = express();
 
+const projectsPath = path.join(__dirname, `projects`);
+
 api.use(nocache());
 api.use(cors({ origin: "*" }));
 api.use(bodyParser.json());
-api.use("/levels", express.static("levels"));
+api.use("/projects", express.static(projectsPath));
 
 api.get("/", function (req, res) {
   const { version } = require("./package.json");
@@ -21,144 +23,110 @@ api.get("/", function (req, res) {
 
 const isDir = (path) => fs.lstatSync(path).isDirectory();
 
-api.get("/levels", function (req, res) {
-  const levelsPath = path.join(__dirname, `levels`);
+const PROJECT_ID_REGEXP = /^[a-zA-Z0-9_-]+$/;
 
-  const levels = fs.readdirSync(levelsPath).reduce((acc, id) => {
-    const levelPath = path.join(levelsPath, id, `level.json`);
+api.get("/projects", function (req, res) {
+  try {
+    const projects = fs.readdirSync(projectsPath).reduce((acc, id) => {
+      const projectPath = path.join(projectsPath, id, `data.json`);
 
-    if (fs.existsSync(levelPath)) {
-      const levelData = JSON.parse(fs.readFileSync(levelPath));
+      if (fs.existsSync(projectPath)) {
+        const projectData = JSON.parse(fs.readFileSync(projectPath));
 
-      if (isDir(path.join(levelsPath, id))) {
-        acc.push({ id: levelData.id, title: levelData.title });
+        if (isDir(path.join(projectsPath, id))) {
+          acc.push({ id: projectData.id, title: projectData.title });
+        }
       }
-    }
-    return acc;
-  }, []);
-
-  res.status(200).json(levels);
-});
-
-api.get("/levels/:levelId", function (req, res) {
-  const { levelId } = req.params;
-
-  const levelsPath = path.join(__dirname, `levels`);
-
-  const levelPath = path.join(levelsPath, levelId, `level.json`);
-
-  if (!fs.existsSync(levelPath)) {
-    res.status(404).json({
-      error: `Could not find level "${levelId}"`,
-    });
-    return;
-  }
-
-  const entitiesPath = path.join(levelsPath, levelId, `entities.json`);
-  const resourcesPath = path.join(levelsPath, levelId, `resources.json`);
-  const roomsPath = path.join(levelsPath, levelId, `rooms`);
-
-  const rooms = fs.readdirSync(roomsPath).reduce((acc, id) => {
-    if (id.endsWith(".save.json")) {
       return acc;
-    }
+    }, []);
 
-    const roomPath = path.join(roomsPath, id);
-
-    if (fs.existsSync(roomPath) && !isDir(roomPath)) {
-      const room = JSON.parse(fs.readFileSync(roomPath));
-
-      acc.push({ id: id.replace(".json", ""), ...room });
-    }
-    return acc;
-  }, []);
-
-  try {
-    res.status(200).json({
-      id: levelId,
-      ...JSON.parse(fs.readFileSync(levelPath)),
-      rooms,
-      entities: JSON.parse(fs.readFileSync(entitiesPath)),
-      resources: JSON.parse(fs.readFileSync(resourcesPath)),
-    });
+    res.status(200).json(projects);
   } catch (e) {
     res.status(500).json({
-      error: `Could not save level "${levelId}" data`,
+      error: `Could not recover projects`,
     });
   }
 });
 
-api.post("/levels/:levelId", function (req, res) {
-  const { levelId } = req.params;
+api.get("/projects/:projectId", function (req, res) {
+  const { projectId } = req.params;
 
-  const levelsPath = path.join(__dirname, `levels`);
-  const levelPath = path.join(levelsPath, levelId, `level.json`);
-  const levelCopyPath = path.join(levelsPath, levelId, `level.save.json`);
-
-  if (!fs.existsSync(levelPath)) {
-    res.status(404).json({
-      error: `Could not find level "${levelId}"`,
-    });
-    return;
-  }
-
-  const { value, error } = schemas.levelSchema.validate(req.body);
-
-  if (error) {
+  if (!PROJECT_ID_REGEXP.test(projectId)) {
     res.status(400).json({
-      error: `Could not validate level "${levelId}" data`,
-      validation: error.details,
+      error: `Invalid project id "${projectId}"`,
     });
     return;
   }
+
+  const projectPath = path.join(projectsPath, projectId, `data.json`);
 
   try {
-    if (process.env.SAVE_COPY === "true")
-      fs.copyFileSync(levelPath, levelCopyPath);
-    fs.writeFileSync(levelPath, JSON.stringify(value, null, 2));
+    if (!fs.existsSync(projectPath)) {
+      res.status(404).json({
+        error: `Could not find project "${projectId}"`,
+      });
+      return;
+    }
 
-    res.status(200).json();
+    res.status(200).json(JSON.parse(fs.readFileSync(projectPath)));
   } catch (e) {
     res.status(500).json({
-      error: `Could not save level "${levelId}" data`,
+      error: `Could not recover project "${projectId}" data`,
     });
   }
 });
 
-api.post("/levels/:levelId/rooms/:roomId", function (req, res) {
-  const { levelId, roomId } = req.params;
+api.post("/projects/:projectId", function (req, res) {
+  const { projectId } = req.params;
 
-  const roomsPath = path.join(__dirname, `levels`, levelId, "rooms");
-
-  const roomPath = path.join(roomsPath, `${roomId}.json`);
-  const roomCopyPath = path.join(roomsPath, `${roomId}.save.json`);
-
-  if (!fs.existsSync(roomPath)) {
-    res.status(404).json({
-      error: `Could not find room "${roomId}" for level "${levelId}"`,
-    });
-    return;
-  }
-
-  const { value, error } = schemas.roomSchema.validate(req.body);
-
-  if (error) {
+  if (!PROJECT_ID_REGEXP.test(projectId)) {
     res.status(400).json({
-      error: `Could not validate room "${roomId}" data for "${levelId}"`,
-      validation: error.details,
+      error: `Invalid project id "${projectId}"`,
     });
     return;
   }
+
+  const projectPath = path.join(projectsPath, projectId, `data.json`);
+  const projectCopyPath = path.join(projectsPath, projectId, `data.save.json`);
 
   try {
-    if (process.env.SAVE_COPY === "true")
-      fs.copyFileSync(roomPath, roomCopyPath);
-    fs.writeFileSync(roomPath, JSON.stringify(value, null, 2));
+    if (!fs.existsSync(projectPath)) {
+      res.status(404).json({
+        error: `Could not find project "${projectId}"`,
+      });
+      return;
+    }
 
-    res.status(200).json();
+    const projectData = JSON.parse(fs.readFileSync(projectPath));
+
+    const { value, error } = schemas.projectSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({
+        error: `Could not validate project "${projectId}" data`,
+        validation: error.details,
+      });
+      return;
+    }
+
+    if (value.version !== projectData.version) {
+      res.status(409).json({
+        error: `Version mismatch for project "${projectId}"`,
+      });
+      return;
+    }
+
+    if (process.env.SAVE_COPY === "true") {
+      fs.copyFileSync(projectPath, projectCopyPath);
+    }
+
+    const updatedLevelData = { ...value, version: value.version + 1 };
+    fs.writeFileSync(projectPath, JSON.stringify(updatedLevelData, null, 2));
+
+    res.status(200).json(updatedLevelData);
   } catch (e) {
     res.status(500).json({
-      error: `Could not save room "${roomId}" data for level "${levelId}"`,
+      error: `Could not save project "${projectId}" data`,
     });
   }
 });
--- schemas.js
+++ schemas.js
@@ -2,14 +2,6 @@ const Joi = require("joi");
 
 const colorSchema = Joi.array().items(Joi.number().min(0).max(1)).length(4);
 
-const levelSchema = Joi.object({
-  id: Joi.string().required(),
-  title: Joi.string().required(),
-  background: colorSchema.required(),
-  gravity: Joi.number().required(),
-  defaultRoomId: Joi.string().required(),
-});
-
 const spawnPointSchema = Joi.object({
   type: Joi.string().required(),
   uuid: Joi.string().required(),
@@ -46,7 +38,7 @@ const eventZoneSchema = Joi.object({
   applyToEntityTypes: Joi.array().items(Joi.string().required()).optional(),
 });
 
-const roomSchema = Joi.object({
+const levelSchema = Joi.object({
   id: Joi.string().required(),
   tileSet: Joi.string().required(),
   tileSize: Joi.number().required(),
@@ -64,8 +56,45 @@ const roomSchema = Joi.object({
   zones: Joi.array().items(eventZoneSchema).required(),
 });
 
+const componentSchema = Joi.object({
+  name: Joi.string().required(),
+  metadata: Joi.any().required(),
+});
+
+const entitySchema = Joi.object({
+  type: Joi.string().required(),
+  components: Joi.array().items(componentSchema).required(),
+});
+
+const projectSchema = Joi.object({
+  id: Joi.string().required(),
+  version: Joi.number().required(),
+  title: Joi.string().required(),
+  background: colorSchema.required(),
+  gravity: Joi.number().required(),
+  defaultLevelId: Joi.string().required(),
+  levels: Joi.array().items(levelSchema).required(),
+  entities: Joi.array().items(entitySchema).required(),
+  assets: Joi.object({
+    sounds: Joi.array().items(
+      Joi.object({
+        src: Joi.string().required(),
+        name: Joi.string().required(),
+      }),
+    ),
+    sprites: Join.array().items(
+      Join.object({
+        src: Joi.string().required(),
+        name: Joi.string().required(),
+        tileWidth: Joi.number().required(),
+        tileHeight: Joi.number().required(),
+      }),
+    ),
+  }).required(),
+});
+
 module.exports = {
+  projectSchema,
   levelSchema,
-  roomSchema,
   spawnPointSchema,
 };
